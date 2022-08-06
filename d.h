86af476321fb1ec850f0ee399597eb23102716c3
Alice <108994354+Alice3636@users.noreply.github.com>
2022-08-06 20:51:40 +0300

    Create d.h

---
+++ d.h
@@ -0,0 +1,19 @@
+#ifndef PRE_ASSEMBLER_H
+#define PRE_ASSEMBLER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+#include "typedef.h"
+#include "functions.h"
+#include <string.h>
+
+#define MAXL 1000
+#define SPACES " \t\r\v\f"
+#define SPACES_AND_NEWLINE " \t\r\v\f\n"
+
+
+
+#endif 
