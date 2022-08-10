25c3a6af7a2a9cf08c1ff1fad04f4efd8c979df6
Alice <108994354+Alice3636@users.noreply.github.com>
2022-08-10 20:49:42 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,7 +1,4 @@
 #include "d.h"
 void yieldError(Error err, int line);
-char * convertfiletochar(const char *filename);
 const char * beautycheck(const char *filename);
-int extraSpaceCheck(const char *filename, int counted);
-int keywordscheck(char *chars);
-int extraCommaCheck(const char *filename,int counted);
+
